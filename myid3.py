c706319334b439982788d6eae01474a5385ba677
Forkyourmother <43376392+Forkyourmother@users.noreply.github.com>
2019-04-30 17:28:25 +0200

    Update myid3.py

--- myid3.py
+++ myid3.py
@@ -24,9 +24,8 @@ class DecisionTree:
 
             
 
-    def train(self, dataframe, class_series, attrs):   
+    def train(self, dataframe, class_series, attrs):
         if len(set(class_series)) == 1:
-            #print(list(class_series.iloc[:1]))
             self.leaf = (class_series.iloc[0])
             return self
         
@@ -43,14 +42,14 @@ class DecisionTree:
             column_entropy = {}
             for attribute in attrs:
                 entropy = 0     
-                for value in list(set(dataframe[attribute])):       #kanske är bra med attributes
+                for value in list(set(dataframe[attribute])):
                     Index = dataframe.index[dataframe[attribute] == value].tolist() 
                     indexed_class = class_series.loc[Index]
                     class_counts = indexed_class.value_counts()
                     entropy_slice = np.sum(-np.log2(class_counts/len(indexed_class)) * class_counts/len(indexed_class)) * len(dataframe[attribute]) / len(indexed_class)
                     entropy += entropy_slice        
                 column_entropy[attribute] = entropy
-            next_node = min(column_entropy.items(), key = lambda x: x[1])[0]   
+            next_node = min(column_entropy.items(), key = lambda x: x[1])[0]  
 
             self.root = next_node
 
@@ -58,24 +57,24 @@ class DecisionTree:
             Index = dataframe.index[dataframe[next_node] == value].tolist()
             attrs2 = [x for x in attrs if x not in next_node]
             if len(attrs) >= 2:
-                dataframe2 = dataframe.drop(next_node, axis = 1).loc[Index] #make these two just one line
-                #dataframe2 = dataframe2.loc[Index]
+                dataframe2 = dataframe.drop(next_node, axis = 1).loc[Index]
             else:
                 dataframe2 = dataframe.loc[Index]
             class_series2 = class_series.loc[Index]
             instance = DecisionTree().train(dataframe2, class_series2, attrs2)
 
             self.tree.append((next_node, value, instance))
+            # print(next_node, value, instance)
+            # # print(self.tree)
 
         return self
-
 
     def predict(self, instance, decisiontree):
         if decisiontree.leaf != "Nothing":  #This is the reason that leaf is set to a string instead of None in the class by default. 
             return decisiontree.leaf             #otherwise we encounter the error "The truth value of a series is ambiguous..." and can't solve this by
-        value = int(instance[decisiontree.root]) #remaking the series-value into a string
+        value = instance[decisiontree.root] #remaking the series-value into a string
         for node in decisiontree.tree:
-            if node[1] == value:
+            if str(node[1]) == value:
                 newdecisiontree = node[2]
                 return self.predict(instance, newdecisiontree)
 
@@ -115,16 +114,17 @@ class DecisionTree:
             return str(Treedict)
 
     def make_tree_dictionary(self, trained_class):
+        nr_to_string_dict = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five"}
         if trained_class.leaf != "Nothing":
             return trained_class.leaf
         subdict = {}
         if trained_class.root != None:
             treelist = []
             subtree = trained_class.tree
+            print(subtree)
             attribute = trained_class.root
             for i in range(len(subtree)):
-                treelist.append([i + 1, self.make_tree_dictionary(subtree[i][2])])
-            #print(treelist)
+                treelist.append([nr_to_string_dict[i + 1], self.make_tree_dictionary(subtree[i][2])])
             subdict[attribute] = treelist
         return subdict               #Since I actually used instances of a class for my decision tree the simplest way of getting a readable string out of
                                      #of the tree was using a mis of dictionaries and lists where the attributes are dictionary keys and the attribute 
@@ -132,4 +132,3 @@ class DecisionTree:
 
     def save(self, output):
         dill.dump(self, output)
-
